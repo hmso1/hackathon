c81e7d1f5c26ac9b7aef32c65f461218c1e5aa7c
Iris So <iris_so@sasa.com>
2021-05-20 17:11:21 +0800

    finish compressed code

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
-const {src, dest} = require('gulp');
+const {src, dest, series} = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const rename = require('gulp-rename');
-const minify = require("gulp-babel-minify");
 
+const babel = require('gulp-babel')
+const uglify = require("gulp-uglify");
 
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass')
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano')
 
 function compileHTML() {
   return src('src/*.html')
@@ -12,16 +18,39 @@ function compileHTML() {
 }
 
 function compileJS() {
-  return src("src/js/typed.js")
-    .pipe(minify({
-      mangle: {
-        keepClassName: true
-      }
-    }))
+  return src("src/js/*.js")
+    .pipe(babel())
+    .pipe(uglify())
     .pipe(dest("js"));
+}
+
+function compileSlickJS() {
+  return src("src/js/slick/*.js")
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(dest("js/slick"));
+}
+
+function compilePrefixerSCSS() {
+  return src("src/css/*.scss")
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss([autoprefixer(), cssnano()]))
+  .pipe(dest("css"));
+}
+
+function compilePrefixerCSS() {
+  return src("src/css/*.css")
+  .pipe(postcss([autoprefixer(), cssnano()]))
+  .pipe(dest("css"));
+}
+
+function compilePrefixerSlickCSS() {
+  return src("src/js/slick/*.scss")
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss([autoprefixer(), cssnano()]))
+  .pipe(dest("js/slick"));
 }
 
 
 
-
-exports.default = compileJS
+exports.default = series(compilePrefixerCSS, compilePrefixerSCSS, compilePrefixerSlickCSS)
